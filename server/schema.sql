d04253b9fc483197561dd795ef0c264b4654e5f4
mkdir-helen <experiment.helen@gmail.com>
2018-12-31 17:34:40 -0500

    created basic schema

---
+++ server/schema.sql
@@ -0,0 +1,13 @@
+create table users(
+    id serial primary key,
+    name varchar(50) not null,
+    email varchar(50) not null unique,
+    username varchar(50) not null unique,
+    password varchar(100) not null,
+);
+create table images(
+    id serial primary key,
+    name text,
+    description text,
+    user_id integer references users(id)
+);
