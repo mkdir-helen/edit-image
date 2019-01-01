3e121bb86856b2342a984bf017ce720c5b2eca69
mkdir-helen <experiment.helen@gmail.com>
2019-01-01 18:16:21 -0500

    attempted to import cloudinary

--- server/server.js
+++ server/server.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const multer = require('multer');
 const db = require('./models/db');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, '/uploads')
+  },
+  filename: function(req, file, cb) {
+    console.log(file)
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+});
+
+app.post('/upload', (req, res, next) => {
+  const upload = multer({ storage }).single('name-of-input-key')
+  upload(req, res, function(err) {
+    if (err instanceof multer.MulterError) {
+      return res.send(err);
+    }else if(err){
+      return res.send(err);
+    }
+    res.json(req.file);
+  })
+})
 
 app.listen(5000, () => {
     console.log('listening on port 5000');
