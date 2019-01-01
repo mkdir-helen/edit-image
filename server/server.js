289e880186f4434de6f1a76e69ef8acaac51b978
mkdir-helen <experiment.helen@gmail.com>
2019-01-01 16:32:30 -0500

    Added more dependenices, database, and server

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const db = require('./models/db');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(5000, () => {
+    console.log('listening on port 5000');
+  });
