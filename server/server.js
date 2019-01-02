ed80e9af4f4337d9444a7b00460bba861387bd24
mkdir-helen <experiment.helen@gmail.com>
2019-01-02 17:04:21 -0500

    now able to change the filename through the form input

--- server/server.js
+++ server/server.js
@@ -18,7 +18,10 @@ app.get('/', (req, res) => {
 
 app.post('/upload', upload.single('image'), async (req, res) => {
   console.log(req.file);
-  const result = await cloudinary.v2.uploader.upload(req.file.path);
+  
+  const result = await cloudinary.v2.uploader.upload(req.file.path,{public_id: `melon/${req.body.title}`},
+    function(error, result){console.log(result, error)}
+  );
   // const blog = new Blog()
   // blog.title = req.body.title
   // blog.imageUrl = result.secure_url
@@ -27,6 +30,27 @@ app.post('/upload', upload.single('image'), async (req, res) => {
     message: 'Image is uploaded'
   })
 })
+
+// { public_id: 'melon/baka',
+//   version: 1546464113,
+//   signature: '271de9bf1337ada36ee75b167ba586e13b1b89be',
+//   width: 602,
+//   height: 617,
+//   format: 'png',
+//   resource_type: 'image',
+//   created_at: '2019-01-02T21:21:53Z',
+//   tags: [],
+//   bytes: 190350,
+//   type: 'upload',
+//   etag: '4a3b1b0b8a9bfb950a3c97e0e7f327d7',
+//   placeholder: false,
+//   url:
+//    'http://res.cloudinary.com/melonimage/image/upload/v1546464113/melon/baka.png',
+//   secure_url:
+//    'https://res.cloudinary.com/melonimage/image/upload/v1546464113/melon/baka.png',
+//   access_mode: 'public',
+//   original_filename: 'f9ee673da94761fd918c35f5655f60d9' }
+
 
 // file:
 //    { fieldname: 'image',
