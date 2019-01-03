72acf7c1de2d287301b2743b48a13dd7f42d97c2
mkdir-helen <experiment.helen@gmail.com>
2019-01-03 16:46:10 -0500

    deleted an unnecessary comma

--- server/schema.sql
+++ server/schema.sql
@@ -3,7 +3,7 @@ create table users(
     name varchar(50) not null,
     email varchar(50) not null unique,
     username varchar(50) not null unique,
-    password varchar(100) not null,
+    password varchar(100) not null
 );
 create table images(
     id serial primary key,
