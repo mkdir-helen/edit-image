1e0a4b1df86e604cf00cecf4783a2872d4c52077
mkdir-helen <experiment.helen@gmail.com>
2019-01-08 09:30:28 -0500

    Added nav component

---
+++ client/src/components/Nav.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+export default class Nav extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/login">Login</Link>
+            </li>
+            <li>
+                <Link to="/register">Register</Link>
+            </li>
+            <li>
+                <Link to="/edit">Edit</Link>
+            </li>
+            <li>
+                <Link to="/gallery">Gallery</Link>
+            </li>
+            <li>
+                <Link to="/crop">Crop</Link>
+            </li>
+        </ul>
+      </div>
+    )
+  }
+}
