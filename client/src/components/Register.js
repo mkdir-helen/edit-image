1028167b1d8303c168a64d39731baef38d4da92d
mkdir-helen <experiment.helen@gmail.com>
2019-01-08 19:31:40 -0500

    Created login/register component

---
+++ client/src/components/Register.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+
+export default class Register extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            username: '',
+            password: ''
+        };
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        fetch(`/register`, {
+            method: 'POST',
+            body: JSON.stringify({
+                name: this.state.name,
+                email: this.state.email,
+                username: this.state.username,
+                password: this.state.password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(r => r.json())
+        .then(result => {
+            console.log(result);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }
+    handleName = (e) => {
+        this.setState({name: e.target.value});
+    }
+    handleEmail = (e) => {
+        this.setState({email: e.target.value});
+    }
+    handleUsername = (e) => {
+        this.setState({username: e.target.value});
+    }
+    handlePassword = (e) => {
+        this.setState({password: e.target.value});
+    }
+    render(){
+        return (
+            <form action='/register' 
+            method='POST' 
+            className='registerForm'
+            onSubmit={this.handleSubmit}
+            >
+                <h3>Sign up:</h3>
+                <label>Name: </label>
+                <input type='text' 
+                name='name'
+                onChange={this.handleName}
+                value={this.state.name}
+                 />
+                <label>Email: </label>
+                <input type='email' 
+                name='email'
+                onChange={this.handleEmail}
+                value={this.state.email}
+                 />
+                <label>Username: </label>
+                <input type='text'
+                name='username'
+                onChange={this.handleUsername}
+                value={this.state.username}
+                 />
+                <label>Password: </label>
+                <input type='password'
+                name='password'
+                onChange={this.handlePassword}
+                value={this.state.password}
+                 />
+                <button type="submit">Register</button>
+            </form>
+        );
+    }
+}
+
+
+
+
+
