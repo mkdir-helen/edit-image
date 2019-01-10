9cf15fbea71d0724571e315045341c6fa7d55593
mkdir-helen <experiment.helen@gmail.com>
2019-01-10 15:44:27 -0500

    Added uuid to filenames to make it unique

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,56 +1,57 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import 'react-image-crop/dist/ReactCrop.css';
-
+import uuid from "uuid";
 
 
 export default class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.imagePreviewCanvasRef = React.createRef();
-        this.cloudinaryImageRef = React.createRef();
-        this.state = {
-            title: '', 
-            image: null,
-            allfiles: [],
-            recentfile: [],
-            recenturl: '',
-            recentname: '',
-            public_id: '',
-            CloudBase64: '',
-            crop: {
-              width: 30,
-              height: 10
-            },
-            imgSrc: '',
-            willEdit: false
-        }
+  constructor(props) {
+    super(props);
+    this.imagePreviewCanvasRef = React.createRef();
+    this.cloudinaryImageRef = React.createRef();
+    this.state = {
+      title: '',
+      image: null,
+      allfiles: [],
+      recentfile: [],
+      recenturl: '',
+      recentname: '',
+      public_id: '',
+      CloudBase64: '',
+      crop: {
+        width: 30,
+        height: 10
+      },
+      imgSrc: '',
+      willEdit: false
     }
+  }
 
 
 
-   onSubmit = (e) => {
-     e.preventDefault();
+  onSubmit = (e) => {
+    e.preventDefault();
     //  debugger;
-    
-     const formdata= new FormData();
-     formdata.append('image', this.state.image);
-     formdata.append('title', this.state.title);
+    const uuidTitle = this.state.title + uuid.v4();
+    console.log(uuidTitle);
+    const formdata = new FormData();
+    formdata.append('image', this.state.image);
+    formdata.append('title', uuidTitle);
     fetch(`/upload`, {
       method: 'POST',
       body: formdata
     })
-    .then(r => r.json())
-    .then(result => {
-      this.setState({
-        allfiles: [result],
-        recentfile: [result[result.length-1]],
-        recenturl: result[result.length-1].url,
-        recentname: result[result.length-1].name,
-        public_id: this.getPublicId(result[result.length-1].url)
-      })
-      
-    });
+      .then(r => r.json())
+      .then(result => {
+        this.setState({
+          allfiles: [result],
+          recentfile: [result[result.length - 1]],
+          recenturl: result[result.length - 1].url,
+          recentname: result[result.length - 1].name,
+          public_id: this.getPublicId(result[result.length - 1].url)
+        })
+
+      });
   }
 
   getImage = (e) => {
@@ -71,52 +72,52 @@ export default class Home extends Component {
     this.props.history.push('/edit');
   }
   handleEditButton = (e) => {
-    this.setState({willEdit: true});
+    this.setState({ willEdit: true });
   }
-  
+
 
   getPublicId = (url) => {
     let arr = url.split('/');
     // console.log(arr);
-    let foldername = arr[arr.length-2];
+    let foldername = arr[arr.length - 2];
     // console.log(foldername);
-    let filename = arr[arr.length-1];
+    let filename = arr[arr.length - 1];
     // console.log(filename);
 
     let public_id_ext = foldername + '/' + filename;
     // console.log(public_id_ext);
-    let public_id = public_id_ext.substring(0, public_id_ext.length-4);
+    let public_id = public_id_ext.substring(0, public_id_ext.length - 4);
     // console.log(public_id);
     return public_id;
   }
-  
+
   render() {
-   
+
     return (
       <div>
-          <h1>Home sweet home</h1>  
-          <h3>Upload an image to start editing!</h3>
-          <form action="/upload" method="post" encType="multipart/form-data" onSubmit={(e) => this.onSubmit(e)}>
-              <div> 
-                  <label htmlFor="title">Name of Image</label>
-                  <input type="text" name="title" id="title" onChange={(e) => this.getTitle(e)} /> 
-              </div>
-              <div>
-                  <label htmlFor="image"></label>
-                  <input type="file" name="image" id="image" onChange={(e) => this.getImage(e)} />
-              </div>
-              <button type="submit">Upload Image</button>
-          </form>
-          { this.state.willEdit &&
-              <button onClick={this.handleEdit}>Edit Image</button>
-          }
-          <div >
-            <img src={this.state.recenturl}
+        <h1>Home sweet home</h1>
+        <h3>Upload an image to start editing!</h3>
+        <form action="/upload" method="post" encType="multipart/form-data" onSubmit={(e) => this.onSubmit(e)}>
+          <div>
+            <label htmlFor="title">Name of Image</label>
+            <input type="text" name="title" id="title" onChange={(e) => this.getTitle(e)} />
+          </div>
+          <div>
+            <label htmlFor="image"></label>
+            <input type="file" name="image" id="image" onChange={(e) => this.getImage(e)} />
+          </div>
+          <button type="submit">Upload Image</button>
+        </form>
+        {this.state.willEdit &&
+          <button onClick={this.handleEdit}>Edit Image</button>
+        }
+        <div >
+          <img src={this.state.recenturl}
             className="homeimage"
             onLoad={this.handleEditButton}
-            />
-          </div>
-          
+          />
+        </div>
+
 
       </div>
     )
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ app.use(bodyParser.json());
 const cloudinary = require('cloudinary');
 const session = require('express-session');
 const pgSession = require('connect-pg-simple')(session);
+const uuidv4 = require('uuid/v4');
 require('./handlers/cloudinary');
 const upload = require('./handlers/multer');
 const db = require('./models/db');
@@ -33,17 +34,17 @@ app.use(
   })
 );
 
-app.use((req,res, next) => {
-  let isLoggedIn = req.session.user ? true: false;
+app.use((req, res, next) => {
+  let isLoggedIn = req.session.user ? true : false;
   console.log(isLoggedIn);
   next();
 });
 
-function protectRoute(req, res, next){
-  let isLoggedIn = req.session.user ? true: false;
-  if(isLoggedIn){
+function protectRoute(req, res, next) {
+  let isLoggedIn = req.session.user ? true : false;
+  if (isLoggedIn) {
     next();
-  }else{
+  } else {
     res.redirect('/login');
   }
 }
@@ -57,36 +58,38 @@ app.post('/upload', upload.single('image'), async (req, res) => {
   console.log(req.file);
   console.log('req.file');
   let date = new Date().toISOString();
-  let title = req.body.title ? req.body.title : req.file.originalname.substring(0, req.file.originalname.length-4);
+  let uuidtitle = req.body.title + uuidv4();
+  let uuidnontitle = req.file.originalname.substring(0, req.file.originalname.length - 4) + uuidv4();
+  let title = req.body.title ? req.body.title : uuidnontitle;
   let folder = req.session.user ? req.session.user.username : 'demo';
-  const result = await cloudinary.v2.uploader.upload(req.file.path,{public_id: `${folder}/${title}`},
-  function(error, result){console.log(result, error)}
+  const result = await cloudinary.v2.uploader.upload(req.file.path, { public_id: `${folder}/${title}` },
+    function (error, result) { console.log(result, error) }
   );
-  if(req.session.user){
+  if (req.session.user) {
     Image.addImage(title, result.secure_url, req.session.user.id)
-    .then(result => {console.log(result); res.redirect('/edit');});
-  }else{
+      .then(result => { console.log(result); res.redirect('/edit'); });
+  } else {
     Demo.addDemo(title, folder, result.secure_url, null)
-    .then(result => {console.log(result); res.redirect('/edit');});
+      .then(result => { console.log(result); res.redirect('/edit'); });
   }
 });
 
 app.post('/update', upload.single('image'), async (req, res) => {
-  
-  const result = await cloudinary.v2.uploader.upload(req.file.path,{public_id: req.body.public_id},
-    function(error, result){console.log(result, error)}
+
+  const result = await cloudinary.v2.uploader.upload(req.file.path, { public_id: req.body.public_id },
+    function (error, result) { console.log(result, error) }
   );
   // const result = await cloudinary.v2.uploader.upload(req.file.path,
   //   {public_id: req.body.public_id, invalidate: true},
   //   function(error, result) {console.log(result, error)});
 
-  if(req.session.user){
+  if (req.session.user) {
     Image.addImage(req.body.title, result.secure_url, req.session.user.id)
       .then(Image.getByUser(req.session.user.id)
         .then(result => {
           res.send(result);
         }))
-  }else{
+  } else {
     Demo.addDemo(req.body.title, req.body.folder, result.secure_url, null)
       .then(
         Demo.getAll()
@@ -101,14 +104,14 @@ app.post('/delete', (req, res) => {
   console.log('console loggin important things!!!!!');
   console.log(req.body.publicID);
   console.log(req.body.url);
-  cloudinary.v2.uploader.destroy(`${req.body.publicID}`, 
-    {invalidate: true }, function(error, result) {console.log(result, error)});
-  if(req.session.user){
+  cloudinary.v2.uploader.destroy(`${req.body.publicID}`,
+    { invalidate: true }, function (error, result) { console.log(result, error) });
+  if (req.session.user) {
     Image.deleteByUrl(req.body.url)
       .then(result => {
         res.send(result);
       })
-  }else{
+  } else {
     Demo.deleteByUrl(req.body.url)
       .then(result => {
         res.send(result);
@@ -117,9 +120,9 @@ app.post('/delete', (req, res) => {
 })
 
 app.get('/edit', (req, res) => {
-  if(req.session.user){
+  if (req.session.user) {
     res.redirect(`/${req.session.user.username}/edit`);
-  }else{
+  } else {
     Demo.getAll()
       .then(result => {
         res.send(result);
@@ -134,9 +137,9 @@ app.get('/:user/edit', protectRoute, (req, res) => {
 })
 
 app.get('/active', (req, res) => {
-  if(req.session.user){
+  if (req.session.user) {
     res.send(true);
-  }else{
+  } else {
     res.send(false);
   }
 })
@@ -146,7 +149,7 @@ app.get('/gallery', protectRoute, (req, res) => {
   res.redirect(`/${req.session.user.username}/gallery`);
 });
 
-app.get('/:user/gallery', protectRoute, (req,res) => {
+app.get('/:user/gallery', protectRoute, (req, res) => {
   console.log(req.params.user);
   Image.getByUser(req.session.user.id)
     .then(result => {
@@ -176,8 +179,8 @@ app.delete('/photo/:photoID', protectRoute, (req, res) => {
       console.log(result);
       const pubID = publicID(result.url);
       console.log(pubID);
-      cloudinary.v2.uploader.destroy(`${pubID}`, 
-    {invalidate: true }, function(error, result) {console.log(result, error)});
+      cloudinary.v2.uploader.destroy(`${pubID}`,
+        { invalidate: true }, function (error, result) { console.log(result, error) });
     })
   Image.deleteById(req.params.photoID)
     .then(result => {
@@ -196,43 +199,43 @@ app.delete('/photo/:photoID', protectRoute, (req, res) => {
 //   res.send('edit photo');
 // });
 
-app.get('/login', (req,res) =>{
+app.get('/login', (req, res) => {
   res.send(loginForm());
 })
 
-app.post('/login', (req,res) => {
+app.post('/login', (req, res) => {
   User.getByUsername(req.body.username)
     .then(user => {
       let didMatch = user.checkPassword(req.body.password, user.password);
-      if(didMatch){
+      if (didMatch) {
         req.session.user = user;
         console.log(req.session.user);
         // req.session.returnTo = req.originalUrl;
         // console.log(req.session.returnTo);
         res.redirect('/gallery');
-      }else{
+      } else {
         res.redirect('/login');
       }
     });
 });
 
-app.get('/register', (req,res) => {
+app.get('/register', (req, res) => {
   res.send(registerForm());
 })
 
-app.post('/register', (req,res) => {
+app.post('/register', (req, res) => {
   User.addUser(
     req.body.name,
     req.body.email,
     req.body.username,
     req.body.password
   )
-  .then(user => {
-    req.session.user = user;
-    // console.log(req.session);
-    // req.session.returnTo = req.originalUrl;
-    res.redirect('/gallery');
-  })
+    .then(user => {
+      req.session.user = user;
+      // console.log(req.session);
+      // req.session.returnTo = req.originalUrl;
+      res.redirect('/gallery');
+    })
 });
 
 
@@ -242,7 +245,7 @@ app.post('/register', (req,res) => {
 
 
 
-app.post('/logout', (req,res)=> {
+app.post('/logout', (req, res) => {
   req.session.destroy();
   // req.session.returnTo = null;
 
@@ -316,5 +319,5 @@ app.post('/logout', (req,res)=> {
 // })
 
 app.listen(5000, () => {
-    console.log('listening on port 5000');
-  });
+  console.log('listening on port 5000');
+});
