f4d1c89d3bc46a4c3095804a2e8c7d3d91d7e022
mkdir-helen <experiment.helen@gmail.com>
2019-01-15 16:40:39 +0000

    update build

---
+++ server/public/precache-manifest.7e749614001c60249c0b7398c3a2ba7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dce8725ce7a97ef1fe3",
+    "url": "/static/css/main.e559aa94.chunk.css"
+  },
+  {
+    "revision": "1dce8725ce7a97ef1fe3",
+    "url": "/static/js/main.1dce8725.chunk.js"
+  },
+  {
+    "revision": "cad8315e3b3c1ced1c98",
+    "url": "/static/css/1.20fe6a0c.chunk.css"
+  },
+  {
+    "revision": "cad8315e3b3c1ced1c98",
+    "url": "/static/js/1.cad8315e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0f7ab98a104ea9515f5957f252c22563",
+    "url": "/index.html"
+  }
+];
