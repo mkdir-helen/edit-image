0bc6bea736a622b0f62113da874b1ff1e120dda9
mkdir-helen <experiment.helen@gmail.com>
2019-01-15 18:19:45 -0500

    add it

---
+++ server/public/precache-manifest.08083aa10c1745113b383ff845384f77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "466b20d422c4ae24071c",
+    "url": "/static/js/main.466b20d4.chunk.js"
+  },
+  {
+    "revision": "d3c36354483399a4b456",
+    "url": "/static/js/1.d3c36354.chunk.js"
+  },
+  {
+    "revision": "466b20d422c4ae24071c",
+    "url": "/static/css/main.ef1c9db2.chunk.css"
+  },
+  {
+    "revision": "d3c36354483399a4b456",
+    "url": "/static/css/1.6e38c2c6.chunk.css"
+  },
+  {
+    "revision": "b030cf58dc449fd50bd7a55f34be2053",
+    "url": "/index.html"
+  }
+];
